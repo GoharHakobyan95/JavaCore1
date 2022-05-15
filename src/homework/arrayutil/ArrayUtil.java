d4e25572a8d8debdd6a44ac553f53e63181f7099
Gohar Hakobyan <hak.goh95@gmail.com>
2022-05-15 10:16:33 +0400

    Figure Painter commit

---
+++ src/homework/arrayutil/ArrayUtil.java
@@ -0,0 +1,90 @@
+package homework.arrayutil;
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+        //print numbers
+        int[] numbers = {15, 125, 2, -890, 0, 41, 857, -56, 10, 424};
+
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.print(numbers[i] + (", "));
+//        }
+
+
+        //print max from numbers
+//        int max = 0;
+//        for (int i = 0; i < numbers.length; i++) {
+//            if (numbers[i] > max) {
+//                max = numbers[i];
+//            }
+//        }
+//        System.out.println("max number = " + max);
+
+
+        //print min from numbers
+//        int min = numbers.length;
+//        for (int i = 0; i < numbers.length; i++) {
+//            if (numbers[i] < min) {
+//                min = numbers[i];
+//            }
+//        }
+//        System.out.println("min number = " + min);
+
+
+        //print even numbers
+//        int even = 0;
+//        for (int i = 0; i < numbers.length; i++) {
+//            if ((numbers[i] % 2) == 0) {
+//                even = numbers[i];
+//                System.out.println("even number = " + even);
+//            }
+//        }
+
+
+        //print odd numbers
+//        int odd = 0;
+//        for (int i = 0; i < numbers.length; i++) {
+//            if ((numbers[i] % 2) == 1) {
+//                odd = numbers[i];
+//                System.out.println("odd number = " + odd);
+//            }
+//        }
+
+
+        // count of evens & odds;
+//        int countEven = 0;
+//        int countOdd = 0;
+//        for (int i = 0; i < numbers.length; i++) {
+//            if ((numbers[i] % 2) == 0) {
+//                countEven++;
+//            } else {
+//                countOdd++;
+//            }
+//        }
+//        System.out.println("Even number = " + countEven);
+//        System.out.println("Odd number = " + countOdd);
+
+
+        //avarage of nums
+//        int result = 0;
+//        for (int i = 0; i < numbers.length; i++) {
+//            result = result + numbers[i];
+//        }
+//        System.out.println("avarage of nums = " + result / numbers.length);
+
+
+//        //count of array
+//        int result = 0;
+//        for (int i = 0; i < numbers.length; i++) {
+//            result = result + numbers[i];
+//
+//        }
+//        System.out.print("count = " + result);
+
+
+    }
+}
+
+
+
+
+
