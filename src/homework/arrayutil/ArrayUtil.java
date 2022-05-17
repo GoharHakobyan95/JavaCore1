3c9d341aad71e30ce8507db27dec4c89ee3684fd
Gohar Hakobyan <hak.goh95@gmail.com>
2022-05-18 01:05:49 +0400

    ArrayUtil commit

--- src/homework/arrayutil/ArrayUtil.java
+++ src/homework/arrayutil/ArrayUtil.java
@@ -3,16 +3,16 @@ package homework.arrayutil;
 public class ArrayUtil {
     public static void main(String[] args) {
         //print numbers
-        int[] numbers = {15, 125, 2, -890, 0, 41, 857, -56, 10, 424};
+        int[] numbers = {15, 125, 2, -892, 0, 41, 857, -56, 919, 424};
         System.out.print("Numbers = ");
         for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + (",  "));
+            System.out.print(numbers[i] + (", "));
         }
         System.out.println();
 
         //print max from numbers
         int maxValue = 0;
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] > maxValue) {
                 maxValue = numbers[i];
             }
@@ -22,7 +22,7 @@ public class ArrayUtil {
 
         //print min from numbers
         int minValue = numbers.length;
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] < minValue) {
                 minValue = numbers[i];
             }
@@ -31,23 +31,19 @@ public class ArrayUtil {
 
 
         //print even numbers
-        int evenNums = 0;
         System.out.print("Even numbers = ");
         for (int i = 0; i < numbers.length; i++) {
             if ((numbers[i] % 2) == 0) {
-                evenNums = numbers[i];
-                System.out.print(evenNums + ", ");
+                System.out.print(numbers[i] + ", ");
             }
         }
         System.out.println();
 
         //print odd numbers
-        int oddNums = 0;
         System.out.print("Odd numbers = ");
         for (int i = 0; i < numbers.length; i++) {
-            if ((numbers[i] % 2) == 1) {
-                oddNums = numbers[i];
-                System.out.print(oddNums + ", ");
+            if ((numbers[i] % 2) != 0) {
+                System.out.print(numbers[i] + ", ");
             }
         }
         System.out.println();
@@ -66,22 +62,16 @@ public class ArrayUtil {
         System.out.println("Count of odd nums = " + countOfOddNums);
 
 
+        //        //count of array nums
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.print("Count of array nums = " + sum);
+        System.out.println();
         //avarage of array nums
-        int result = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            result = result + numbers[i];
-        }
-        System.out.println("Avarage of nums = " + result / numbers.length);
-
-
-//        //count of array nums
-        int count = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            count = count + numbers[i];
-
-        }
-        System.out.print("Count of array nums = " + result);
-
+        double avg = sum / numbers.length;
+        System.out.println("Avarage of nums = " + avg);
     }
 }
 
