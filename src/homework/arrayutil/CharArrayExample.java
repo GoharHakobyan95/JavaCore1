387d7db6ea58561aa46630a187bf8521b6ce3b41
Gohar Hakobyan <hak.goh95@gmail.com>
2022-05-19 20:09:22 +0400

    CharArrayExample commit

--- src/homework/arrayutil/CharArrayExample.java
+++ src/homework/arrayutil/CharArrayExample.java
@@ -2,7 +2,7 @@ package homework.arrayutil;
 
 public class CharArrayExample {
     public static void main(String[] args) {
-        char[] chars = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e' };
+        char[] chars = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
         char c = 'o';
         int countOfC = 0;
         for (int i = 0; i < chars.length; i++) {
@@ -12,13 +12,13 @@ public class CharArrayExample {
         }
         System.out.println(countOfC);
 
-        char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e' };
-        {
-            System.out.print(chars2[chars2.length / 2 - 1] + (", "));
-            System.out.println(chars2[chars2.length / 2]);
-        }
+        char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
 
-        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'l', 'y' };
+        System.out.print(chars2[chars2.length / 2 - 1] + (", "));
+        System.out.println(chars2[chars2.length / 2]);
+
+
+        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'l', 'y'};
         {
             if (chars3[chars2.length - 1] == 'l' && chars3[chars2.length] == 'y') {
                 System.out.println(true);
@@ -27,25 +27,16 @@ public class CharArrayExample {
             }
         }
 
-        char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'a' };
-        for (int i = 0; i < bobArray.length; i++) {
+        char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'a'};
+        for (int i = 0; i <= 3; i++) {
             if (bobArray[i] == 'b' && bobArray[i + 2] == 'b') {
-                System.out.println(true);
-            } else if (bobArray[0] == 'b' && bobArray[2] == 'b') {
-                System.out.println(true);
-            } else if (bobArray[1] == 'b' && bobArray[3] == 'b') {
-                System.out.println(true);
-            } else if (bobArray[2] == 'b' && bobArray[4] == 'b') {
-                System.out.println(true);
-            } else if (bobArray[3] == 'b' && bobArray[5] == 'b') {
                 System.out.println(true);
             } else {
                 System.out.println(false);
             }
-
         }
 
-        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' ' };
+        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
         for (int j = 0; j < text.length; j++) {
             if (text[j] != ' ') {
                 System.out.print(text[j]);
