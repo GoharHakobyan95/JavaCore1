93afb8ffe3debe74cae7e34bfa0a89967b202785
Gohar Hakobyan <hak.goh95@gmail.com>
2022-05-25 00:20:17 +0400

    CharArraySort commit

--- src/homework/arrayutil/ArraySpaceExample.java
+++ src/homework/arrayutil/ArraySpaceExample.java
@@ -3,22 +3,36 @@ package homework.arrayutil;
 public class ArraySpaceExample {
     public static void main(String[] args) {
         char[] spaceArray = {' ', 'c', 'a', 't', ' ', 'b', 'i', ' ', 'b', ' ', ' '};
-        char[] resultChar = new char[spaceArray.length];
+
+        int startIndex = 0;
+        int endIndex = spaceArray.length - 1;
+
         for (int i = 0; i < spaceArray.length; i++) {
-            if (spaceArray[i] != ' ') {
-                for (int j = spaceArray.length - 1; j > i; j--) {
-                    int tmp=0;
-                    if (spaceArray[j] != ' ') {
-
-                        resultChar[i] = spaceArray[j - i + 1];
-                        for (int k = 0; k < resultChar.length; k++) {
-                        }
-                        System.out.print(resultChar[i]);
-                    }
-
-                }
+            if (spaceArray[startIndex] == ' ') {
+                startIndex++;
+            } else {
+                break;
             }
-
         }
+
+        for (int i = endIndex; i > 0; i--) {
+            if (spaceArray[endIndex] == ' ') {
+                endIndex--;
+            } else {
+                break;
+            }
+        }
+
+        char[] resultChar = new char[endIndex - startIndex + 1];
+        int j = 0;
+        for (int k = startIndex; k <= endIndex; k++) {
+            resultChar[j++] = spaceArray[k];
+        }
+
+        for (char c : resultChar) {
+            System.out.print(c);
+        }
+
     }
+
 }
